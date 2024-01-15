9f98e3cf327700e821dc48282c7db47fbad5d4d4
John Isaac Rizon <jirizon693@gmail.com>
2024-01-15 23:00:38 +0800

    ITS FUCKING WORKING

---
+++ buyingpage.js
@@ -0,0 +1,16 @@
+function openSideBar(){
+    var showsidebar = document.getElementById("sidebar");
+    
+
+if (showsidebar.style.display === "none") 
+{
+showsidebar.style.display = "flex";
+
+
+} else 
+
+{
+showsidebar.style.display = "none";
+}
+
+}
